f7a13b57c973940f4114d50382b55fe122ae4fb2
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-23 20:53:12 +0200

    Configure database connection

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 api = Flask(__name__)
 
+api.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(api)
 
 @api.route("/")
 def hello():
     return "Hello World (again) from Flask in a uWSGI Nginx Docker container with \
      Python 3.7 (from the example template)"
 
+
 @api.route("/status")
 def status_page():
-    return "UP - Running"
+    return "UP -------"
 
 
 if __name__ == "__main__":
