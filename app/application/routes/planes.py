b9a4c4b21c62d3e7812cfd5fb4fd717ac21dd057
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-04-28 08:30:31 +0200

    :art: Endpoint should call use-case not datasources

--- app/application/routes/planes.py
+++ app/application/routes/planes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 
 from application.connection import db
+from business.interactors import list_all_available_planes
 from infrastructure.datasource import PlaneDatasource
 from infrastructure.transport import PlaneSerializer
 
@@ -10,6 +11,6 @@ planes = Blueprint('plane_api', __name__, url_prefix='/')
 @planes.route("/planes", methods=['GET'])
 def get_all_planes():
     plane_datasource = PlaneDatasource(session=db.session)
-    planes = plane_datasource.get_all_planes()
+    planes = list_all_available_planes(plane_repository=plane_datasource)
 
     return jsonify(PlaneSerializer.to_json(planes))
