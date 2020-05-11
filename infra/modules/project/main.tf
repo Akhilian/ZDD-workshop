b82545502ad941f687d9005161804cf07b80b744
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-05-11 13:52:56 +0200

    :building_construction: Add a ressource for database

---
+++ infra/modules/project/main.tf
@@ -0,0 +1,12 @@
+resource "aws_db_instance" "database" {
+  engine         = "postgres"
+  engine_version = "10.12"
+
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  instance_class       = "db.t2.micro"
+  name                 = "plane_tracker"
+  username             = "captain"
+  password             = "captainsully"
+  parameter_group_name = "default.postgres10"
+}
